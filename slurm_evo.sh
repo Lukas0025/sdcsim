d48ba7e21603ef36c7b3c331fa09b354c614eca8
Lukáš Plevač <lukas@plevac.eu>
2024-04-21 09:17:29 +0200

    Added working CaUnnaryAdd automaton

--- slurm_evo.sh
+++ slurm_evo.sh
@@ -5,7 +5,7 @@
 #
 #SBATCH -J sdcsim
 #SBATCH -n 1
-#SBATCH -t 10:00:00
+#SBATCH -t 48:00:00
 #SBATCH -p long
 #
 
@@ -17,8 +17,14 @@ echo $SLURM_TASKS_PER_NODE
 # load module enviroment
 . modules.sh
 
-# load python
-ml Python
-ml Makefile
-
-python evo.py
+for pmut in 1 2 4 8 16 32 64 100
+do
+    for mgenes in 1 2 3 4
+    do
+        for drand in 1 5 10 100 500
+        do
+            echo "python evo.py --pop 8 --pmut $pmut --mgenes $mgenes --gen 300 --drand $drand --profile"
+            python evo.py --pop 8 --pmut $pmut --mgenes $mgenes --gen 300 --drand $drand --profile --tour 2
+        done
+    done
+done
